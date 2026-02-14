0f2afda266defe87b109a0a3c8e5f6c0f28751e5
Clarence Dan <tangdan0854@gmail.com>
2026-02-15 00:11:18 +0800

    Fix fmt.Sprintf issues found by go vet

--- relay/channel/coze/relay-coze.go
+++ relay/channel/coze/relay-coze.go
@@ -207,7 +207,7 @@ func handleCozeEvent(c *gin.Context, event string, data string, responseText *st
 			return
 		}
 
-		common.SysError(fmt.Sprintf("stream event error: ", errorData.Code, errorData.Message))
+		common.SysError(fmt.Sprintf("stream event error: code=%v message=%v", errorData.Code, errorData.Message))
 	}
 }
 
